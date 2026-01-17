5c1662ea6ec0f84e5d4f4be7ef775240c30461e9
Thomas Emilson <thomasemilson@hotmail.com>
2026-01-18 00:14:45 +0100

    Fix another error in relation to zustand

--- src/state/projectActions/index.ts
+++ src/state/projectActions/index.ts
@@ -3,14 +3,15 @@ import { createBoardActions } from "./boardActions";
 import { createCoreActions } from "./coreActions";
 import { createObjectActions } from "./objectActions";
 import { createSquadActions } from "./squadActions";
-import type { ProjectStore } from "./types";
+import type { ProjectActions, ProjectStore } from "./types";
 
 export type { ProjectActions, ProjectStateSlice, ProjectStore } from "./types";
 
 export const createProjectActions: StateCreator<
   ProjectStore,
   [["zustand/immer", never]],
-  []
+  [],
+  ProjectActions
 > = (set, get) => ({
   ...createCoreActions(set, get),
   ...createBoardActions(set, get),
