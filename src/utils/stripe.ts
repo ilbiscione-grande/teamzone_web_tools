137b1ad4f8b08fefc4f81ce71683f4c75ca69c2d
Thomas Emilson <thomasemilson@hotmail.com>
2026-01-18 01:31:50 +0100

    Stripe/minor update and bugs fixes

--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -2,6 +2,4 @@ import Stripe from "stripe";
 
 const stripeSecret = process.env.STRIPE_SECRET_KEY ?? "";
 
-export const stripe = stripeSecret
-  ? new Stripe(stripeSecret, { apiVersion: "2024-11-20.acacia" })
-  : null;
+export const stripe = stripeSecret ? new Stripe(stripeSecret) : null;
